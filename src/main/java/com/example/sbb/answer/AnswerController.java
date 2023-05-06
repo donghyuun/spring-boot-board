60af980ef2c0824be4032a3adf5aec33ccb0e5ca
donghyuun <bomul0524@gmail.com>
2023-05-06 21:57:31 +0900

    3-09 답변글 수정, 삭제 기능

--- src/main/java/com/example/sbb/answer/AnswerController.java
+++ src/main/java/com/example/sbb/answer/AnswerController.java
@@ -5,14 +5,18 @@ import com.example.sbb.user.UserService;
 import com.example.sbb.question.Question;
 import com.example.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.security.Principal;
 
 @RequestMapping("/answer")
@@ -38,4 +42,43 @@ public class AnswerController {
         return String.format("redirect:/question/detail/%s", id);//제자리 유지
     }
 
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String answerModify(AnswerForm answerForm, @PathVariable("id") Integer id, Principal principal){
+        Answer answer = this.answerService.getAnswer(id);
+        if(!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+
+        answerForm.setContent(answer.getContent());
+        return "answer_form";//AnswerForm 객체를 템플릿에 같이 넘겨줌
+    }
+
+    @PreAuthorize("isAuthenticated()")//로그인이 필요한 메서드로 설정
+    @PostMapping("/modify/{id}")
+    public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult, @PathVariable("id") Integer id, Principal principal){
+        if(bindingResult.hasErrors()){
+            return "answer_form";
+        }
+
+        Answer answer = this.answerService.getAnswer(id);
+        if(!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.answerService.modify(answer, answerForm.getContent());
+        //답변 삭제 후 해당 답변이 있는 기존 페이지로 복귀 (해당 답변의 질문 id를 이용)
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")//로그인이 필요한 메서드로 설정
+    @GetMapping("/delete/{id}")
+    public String answerDelete(Principal principal, @PathVariable("id") Integer id){
+        Answer answer = this.answerService.getAnswer(id);
+        if(!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        this.answerService.delete(answer);
+        //답변 삭제 후 해당 답변이 있던 기존 페이지로 복귀 (해당 답변의 질문 id를 이용)
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    }
 }
--- src/main/java/com/example/sbb/answer/AnswerService.java
+++ src/main/java/com/example/sbb/answer/AnswerService.java
@@ -1,17 +1,30 @@
 package com.example.sbb.answer;
 
+import com.example.sbb.DataNotFoundException;
 import com.example.sbb.question.Question;
 import com.example.sbb.user.SiteUser;//답변 시 작성자를 저장할 수 있도록
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class AnswerService {
 
     private final AnswerRepository answerRepository;
+
+    public Answer getAnswer(Integer id){
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if(answer.isPresent()){
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    //답변 등록
     public void create(Question question, String content, SiteUser author){//answer 객체를 생성하여 저장
         Answer answer = new Answer();
         answer.setContent(content);
@@ -19,6 +32,18 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+
+    //답변 수정
+    public void modify(Answer answer, String content){
+        answer.setContent(content);
+        answer.setModifyData(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    //답변 삭제
+    public void delete(Answer answer){
+        this.answerRepository.delete(answer);
     }
 
 }
